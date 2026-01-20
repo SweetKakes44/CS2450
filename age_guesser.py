9e9d28f708f92383e0cbdbcda2c03843d7981d89
Natahlya <kkuhlmann.natahlya@gmail.com>
2026-01-20 10:33:27 -0700

    fixed some bugs with quotes

--- age_guesser.py
+++ age_guesser.py
@@ -1,13 +1,13 @@
 import random
 
-print("Welcome! I am the age guesser. I'm going to try to guess your age.)
-name = input(What's your name? ")
+print("Welcome! I am the age guesser. Im going to try to guess your age.")
+name = input("Whats your name? ")
 
 while True:
     age_guess = random.randint(14, 40)
     answer = input(f"Is your age {age_guess}? (y/n): ").strip().lower()
 
-    if answer == y:
+    if answer == 'y':
         print(f"Hooray! {name} is {age_guess} years old.")
         break
     else:
