ad5c90a702c6e047b9925eaccfa2429491abc487
Natahlya <natahlya@DESKTOP-K9GCV17.localdomain>
2026-01-20 10:20:01 -0700

    changed wording and the age range of the guess

--- age_guesser.py
+++ age_guesser.py
@@ -1,10 +1,10 @@
 import random
 
-print("Hello! Im going to try to guess your age.)
-name = input(Whats your name? ")
+print("Welcome! I am the age guesser. I'm going to try to guess your age.)
+name = input(What's your name? ")
 
 while True:
-    age_guess = random.randint(15, 30)
+    age_guess = random.randint(14, 40)
     answer = input(f"Is your age {age_guess}? (y/n): ").strip().lower()
 
     if answer == y:
